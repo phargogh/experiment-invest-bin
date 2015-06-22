b6407960e22e60cd3a07ce523867e72fd5997ba2
James Douglass <jdouglass@stanford.edu>
2015-06-22 10:47:48 -0700

    Adding a script to commit branches to this repo.

---
+++ get_branches.sh
@@ -0,0 +1,15 @@
+invest3repo=../../invest-natcap.invest-3
+
+for branchname in `hg heads --template="{branch}\n" -R $invest3repo`
+do
+    if [ "$branchname" = "default" ] || [ "$branchname" = "master" ] || [ "`echo $branchname | grep -o feature/`" = "feature/" ]
+    then
+        target_branch=$branchname
+    else
+        target_branch=feature/$branchname
+    fi
+    hg up -r $target_branch -R $invest3repo
+    hg branch $target_branch -R .
+    ./get.sh
+    hg commit -m "Copying branch $branchname to natcap/invest:$target_branch"
+done
